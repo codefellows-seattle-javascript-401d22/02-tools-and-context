af83bc1e7fc491fc9b554005f3e5ac4e08dee361
nicoleweese <nweese@ymail.com>
2018-02-13 15:57:31 -0800

    solution finished

--- lab-nicole/__test__/fp.test.js
+++ lab-nicole/__test__/fp.test.js
@@ -4,10 +4,66 @@ const fp = require('../lib/fp.js');
 require('jest');
 
 describe('Functional Programming Module', function() {
+
   describe('#map', function() {
     it('should throw error array not provided if array is not provided', function() {
-      expect(map()).toThrow();
-      expect(map()).toThrow('array not provided');
+      expect(fp.map).toThrow('array not provided');
+    });
+    it('should throw error no callback function provided if no callback is provided', function() {
+      expect(() => {
+        fp.map('wooow', [21, 23])
+      }).toThrow('no callback function provided');
+    })
+    it('should return [4, 6, 8]', function() {
+      expect(fp.map(function(n){
+        return n * 2;
+      }, [2, 3, 4])).toEqual([4, 6, 8]);
+    })
+  })
+
+  describe('#filter', function() {
+    it('should throw error array not provided if array is not provided', function() {
+      expect(fp.filter).toThrow('array not provided');
+    });
+    it('should throw error no callback function provided if no callback is provided', function() {
+      expect(() => {
+        fp.filter('wooow', [3, 6, 0])
+      }).toThrow('no callback function provided');
+    })
+    it('should return [3, 5, 6, 10]', function() {
+      expect(fp.filter(function(n) {
+        return n !== 4;
+      }, [4, 3, 5, 4, 6, 10, 4])).toEqual([3, 5, 6, 10]);
+    })
+  })
+
+  describe('#reduce', function() {
+    it('should throw error incorrect number of parameters provided if incorrect number of parameters is provided', function () {
+      expect(fp.reduce).toThrow('incorrect number of parameters provided');
+    });
+    it('should throw error no callback function provided if no callback i provided', function() {
+      expect(() => {
+        fp.reduce('hi', [], [8, 9, 10])
+      }).toThrow('no callback function provided');
+    })
+    it('should return 20', function() {
+      expect(fp.reduce(function(a, b) {
+          return a + b;
+        }, 0, [2, 4, 5, 7, 2])).toEqual(20)
+    })
+  })
+
+  describe('#slice', function() {
+    it('should throw error array not provided if array is not provided', function() {
+      expect(fp.slice).toThrow('array not provided');
+    });
+    it('should throw error values provided were not integers if not passed numbers', function() {
+      expect(() => {
+        fp.slice('hey', 'sup', [2, 4, 5])
+      }).toThrow('values provided were not integers');
+    })
+    it('should return [3, 2, 3]', function() {
+      expect(fp.slice(2, 5, [1, 2, 3, 2, 3])).toEqual([3, 2, 3]);
     })
   })
 })
--- lab-nicole/lib/fp.js
+++ lab-nicole/lib/fp.js
@@ -4,23 +4,24 @@ module.exports = exports = {};
 
 exports.map = function(callback, collection) {
   if(!collection) throw new Error('array not provided');
+  if(typeof callback !== 'function') throw new Error('no callback function provided');
   return Array.prototype.map.call(collection, callback);
 }
 
 exports.filter = function(callback, collection) {
   if(!collection) throw new Error ('array not provided');
+  if(typeof callback !== 'function') throw new Error('no callback function provided');
   return Array.prototype.filter.call(collection, callback);
 }
 
 exports.reduce = function(callback, initialState, collection) {
-  if(!collection) throw new Error('array not provided');
-  if(initialState === undefined) throw new Error('initial state not provided');
+  if(!collection) throw new Error('incorrect number of parameters provided');
+  if(typeof callback !== 'function') throw new Error('no callback function provided');
   return Array.prototype.reduce.call(collection, callback, initialState);
 }
 
 exports.slice = function(begin, end, collection) {
   if(!collection) throw new Error('array not provided');
-  if(begin === undefined) throw new Error('no beginning value provided');
-  if(end === undefined) throw new Error('no ending value provided');
+  if(typeof begin !== 'number' || typeof end !== 'number') throw new Error('values provided were not integers');
   return Array.prototype.slice.call(collection, begin, end);
 }
