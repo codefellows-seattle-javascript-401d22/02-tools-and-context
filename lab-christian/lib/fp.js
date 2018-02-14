7dd1854b66e66c770ffb4fb441038286e924dd44
Christian Miller <christian.ax.miller@gmail.com>
2018-02-13 19:46:55 -0800

    functions completed

--- lab-christian/lib/fp.js
+++ lab-christian/lib/fp.js
@@ -2,18 +2,22 @@
 
 module.exports = exports = {};
 
-exports.map = function() {
-
+exports.map = function(arr, callback) {
+    if (!arr) throw new Error('array not provided');
+    return Array.prototype.map.call(arr, callback);
 }
 
-exports.filter = function() {
-
+exports.filter = function(arr, callback) {
+    if (!arr) throw new Error('array not provided');
+    return Array.prototype.filter.call(arr, callback);
 }
 
 exports.reduce = function() {
-
+    if (!arr) throw new Error('array not provided');
+    return Array.prototype.reduce.call(arr, callback);
 }
 
 exports.slice = function() {
-    
+    if (!arr) throw new Error('array not provided');
+    return Array.prototype.slice.call(arr, callback);
 }
