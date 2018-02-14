1808d89c08a6f60b9a8b509b0d355cc80e2ead43
Christian Miller <christian.ax.miller@gmail.com>
2018-02-13 21:21:57 -0800

    finished tests

--- lab-christian/lib/fp.js
+++ lab-christian/lib/fp.js
@@ -2,22 +2,22 @@
 
 module.exports = exports = {};
 
-exports.map = function(arr, callback) {
+exports.map = function(callback, collection) {
     if (!arr) throw new Error('array not provided');
-    return Array.prototype.map.call(arr, callback);
+    return Array.prototype.map.call(callback, collection);
 }
 
-exports.filter = function(arr, callback) {
+exports.filter = function(callback, collection) {
     if (!arr) throw new Error('array not provided');
-    return Array.prototype.filter.call(arr, callback);
+    return Array.prototype.filter.call(callback, collection);
 }
 
-exports.reduce = function() {
+exports.reduce = function(callback, initialState, collection) {
     if (!arr) throw new Error('array not provided');
-    return Array.prototype.reduce.call(arr, callback);
+    return Array.prototype.reduce.call(callback, initialState, collection);
 }
 
-exports.slice = function() {
+exports.slice = function(begin, end, collection) {
     if (!arr) throw new Error('array not provided');
-    return Array.prototype.slice.call(arr, callback);
+    return Array.prototype.slice.call(begin, end, collection);
 }
