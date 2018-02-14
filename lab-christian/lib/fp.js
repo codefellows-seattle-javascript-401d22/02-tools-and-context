27f7b4f755036c5742e98c7f22bdc29f13ce17de
Christian Miller <christian.ax.miller@gmail.com>
2018-02-13 19:30:12 -0800

    functions laid out

---
+++ lab-christian/lib/fp.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = exports = {};
+
+exports.map = function() {
+
+}
+
+exports.filter = function() {
+
+}
+
+exports.reduce = function() {
+
+}
+
+exports.slice = function() {
+    
+}
