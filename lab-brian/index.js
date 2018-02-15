7cc07da155d600e90bc3968db0706d920e34154f
Brian Bixby <brianbixby0@gmail.com>
2018-02-15 08:52:15 -0800

    done

--- lab-brian/index.js
+++ lab-brian/index.js
@@ -2,7 +2,12 @@
 
 const fp = require('./lib/fp.js');
 
-console.log(fp.map([1,2,3,4], function(n) {console.log('map: ', n*2); return n*2;}));
-console.log(fp.filter([1,2,3,4], function(n) {console.log('filter: ', n>2); return n>2;}));
-console.log(fp.reduce(function(acc, cur) { return cur += acc;}, '', ['man!!!', 'the ', 'is ', 'brian ']));
-console.log('slice function: ', fp.slice([1,2,3,4], 0, 3));   
+// console.log(fp.map([1,2,3,4], function(n) {console.log('map: ', n*2); return n*2;}));
+// console.log(fp.filter([1,2,3,4], function(n) {console.log('filter: ', n>2); return n>2;}));
+// console.log(fp.reduce(function(acc, cur) { return cur += acc;}, '', ['man!!!', 'the ', 'is ', 'brian ']));
+// console.log('slice function: ', fp.slice([1,2,3,4], 0, 3));   
+
+// console.log(fp.map([1,2], function(n) {return n +1;}));
+// console.log(fp.filter([1,2,3,4], function(n) {return n>2;}));
+// console.log(fp.reduce(function(acc, cur) { return cur += acc;}, '', ['man!!!', 'the ', 'is ', 'brian ']));
+console.log('slice function: ', fp.slice([1,2,3,4], 0, 3));  
