c4f54a3de6f335a30072f46737151d931d7d02b1
Ahmed Ali <Ahmed98148@gmail.com>
2018-02-26 16:36:51 -0800

    done

--- lab-ahmed/lib/fp.js
+++ lab-ahmed/lib/fp.js
@@ -3,24 +3,24 @@
 module.exports = exports = {};
 
 exports.map = function(callback, collection){
-  if (!collection) throw new Error('Collection not provided')
-  return Array.prototype.map.call(callback, collection);
-}
+  if (!collection) throw new Error('Collection not provided');
+  return Array.prototype.map.call(collection, callback);
+};
 
 
 exports.filter = function(callback, collection){
-  if (!collection) throw new Error('Collection not provided')
-  return Array.prototype.filter.call(callback, collection);
-}
+  if (!collection) throw new Error('Collection not provided');
+  return Array.prototype.filter.call(collection, callback);
+};
 
 
 exports.reduce = function(callback, initialState, collection){
-  if (!collection) throw new Error('Collection not provided')
-  return Array.prototype.reduce.call(callback, initialState, collection);
-}
+  if (!collection) throw new Error('Collection not provided');
+  return Array.prototype.reduce.call(collection, callback, initialState);
+};
 
 
 exports.slice = function(begin, end, collection){
-  if (!collection) throw new Error('Collection not provided')
+  if (!collection) throw new Error('Collection not provided');
   return Array.prototype.slice.call(begin, end, collection);
-}
+};
